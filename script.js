50bfad8afa6d5927883261e625c17e901a0b61b3
Chris Tai <christopherctai@gmail.com>
2023-05-26 16:14:20 -0700

    Rewrite node factory so the object is more defined

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const nodeFactory = (data, left, right) => {
-    return {data, left, right};
+const Node = (data, left, right) => {
+    return {
+        data: data, 
+        left: left, 
+        right: right
+    }
 } 
+
+
 
