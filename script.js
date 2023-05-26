f2833cc64b948a98abe4ba4e20438d9f8e0db04b
Chris Tai <christopherctai@gmail.com>
2023-05-26 16:48:09 -0700

    Create buildTree function with a way to sort array and remove duplicates

--- script.js
+++ script.js
@@ -6,5 +6,48 @@ const Node = (data, left, right) => {
     }
 } 
 
+const Tree = (array) => {
+    let root; 
+    return {
+        root
+    }
+}
+
+function buildTree(array) {
+    let uniqueSortedArray = mergeSort([...new Set(array)]);
+    
+}
+
+function mergeSort(array) {
+    if (array.length < 2) {
+        return array; 
+    } else {
+        let sortedArray = []; 
+
+        // Sort the left side
+        let arrayLeft = mergeSort(array.slice(0, Math.floor((array.length)/2)));
+
+        // Sort the right side 
+        let arrayRight = mergeSort(array.slice(Math.floor((array.length)/2), array.length));
+
+        let pointerLeft = 0; 
+        let pointerRight = 0;
 
 
+        // merge the two halves
+        for (i = 0; i < array.length; i++) {
+            if (arrayLeft[pointerLeft] < arrayRight[pointerRight] || arrayRight[pointerRight] === undefined) {
+                sortedArray[i] = arrayLeft[pointerLeft];
+                pointerLeft++;
+            } else {
+                sortedArray[i] = arrayRight[pointerRight];
+                pointerRight++;
+            }
+        }
+        return sortedArray; 
+    }
+}
+
+
+let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]; 
+console.log(buildTree(array)); 
