292a564c3fc8a836b47aa7f96763a1840327cbbc
Chris Tai <christopherctai@gmail.com>
2023-05-26 17:57:34 -0700

    Create a function that can build a binary search tree but not a balanced binary search tree

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const util = require('util')
+
 const Node = (data, left, right) => {
     return {
         data: data, 
@@ -9,16 +11,27 @@ const Node = (data, left, right) => {
 const Tree = (array) => { 
     let uniqueSortedArray = mergeSort([...new Set(array)]);  
     return {
+        uniqueSortedArray, 
         root: buildTree(uniqueSortedArray, 0, uniqueSortedArray.length - 1)
     }
 }
 
 function buildTree(array, start, end) {
+    if (start > end) {
+        return null; 
+    }
     let mid = Math.floor((start + end) / 2); 
     let arrayLeft = array.slice(0, mid)
-    let arrayRight = array.slice(mid, end); 
+    let arrayRight = array.slice(mid + 1, end + 1); 
+    return Node(array[mid], buildTree(arrayLeft, 0, arrayLeft.length - 1), buildTree(arrayRight, 0, arrayRight.length - 1));
     
 }
+
+let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]; 
+let sortedArray = [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345];
+console.log(sortedArray.length); 
+console.log(util.inspect(buildTree(sortedArray, 0, sortedArray.length - 1), false, null, true)); 
+
 
 function mergeSort(array) {
     if (array.length < 2) {
@@ -50,6 +63,3 @@ function mergeSort(array) {
     }
 }
 
-
-let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]; 
-console.log(buildTree(array)); 
