584da6d48e36c907265abcc50f911ef23acbf121
Chris Tai <christopherctai@gmail.com>
2023-05-30 19:37:16 -0700

    Refactor script to include prettyPrint function

--- script.js
+++ script.js
@@ -1,65 +1,99 @@
-const util = require('util')
+const util = require("util");
 
+// Node factory
 const Node = (data, left, right) => {
     return {
-        data: data, 
-        left: left, 
-        right: right
-    }
-} 
+        data: data,
+        left: left,
+        right: right,
+    };
+};
 
-const Tree = (array) => { 
-    let uniqueSortedArray = mergeSort([...new Set(array)]);  
-    return {
-        uniqueSortedArray, 
-        root: buildTree(uniqueSortedArray, 0, uniqueSortedArray.length - 1)
-    }
-}
+// Tree factory
+const Tree = (array) => {
+  let uniqueSortedArray = mergeSort([...new Set(array)]);
 
-function buildTree(array, start, end) {
+  const buildTree = (array, start, end) => {
     if (start > end) {
-        return null; 
+      return null;
     }
-    let mid = Math.floor((start + end) / 2); 
-    let arrayLeft = array.slice(0, mid)
-    let arrayRight = array.slice(mid + 1, end + 1); 
-    return Node(array[mid], buildTree(arrayLeft, 0, arrayLeft.length - 1), buildTree(arrayRight, 0, arrayRight.length - 1));
-    
-}
+    let mid = Math.floor((start + end) / 2);
+    let arrayLeft = array.slice(0, mid);
+    let arrayRight = array.slice(mid + 1, end + 1);
+    return Node(
+      array[mid],
+      buildTree(arrayLeft, 0, arrayLeft.length - 1),
+      buildTree(arrayRight, 0, arrayRight.length - 1)
+    );
+  };
 
-let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]; 
-let sortedArray = [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345];
-console.log(sortedArray.length); 
-console.log(util.inspect(buildTree(sortedArray, 0, sortedArray.length - 1), false, null, true)); 
+  const root = buildTree(uniqueSortedArray, 0, uniqueSortedArray.length - 1);
 
+  const insertValue = (value) => {
+    console.log();
+  };
 
+  const deleteValue = (value) => {
+    console.log("hi");
+  };
+
+  return {
+    uniqueSortedArray,
+    root,
+  };
+};
+
+// Experimentation
+let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let tree = Tree(array);
+// console.log(util.inspect(tree.root, false, null, true));
+prettyPrint(tree.root);
+
+// Print a nice looking tree
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+// Sort an array with merge sort
 function mergeSort(array) {
-    if (array.length < 2) {
-        return array; 
-    } else {
-        let sortedArray = []; 
+  if (array.length < 2) {
+    return array;
+  } else {
+    let sortedArray = [];
 
-        // Sort the left side
-        let arrayLeft = mergeSort(array.slice(0, Math.floor((array.length)/2)));
+    // Sort the left side
+    let arrayLeft = mergeSort(array.slice(0, Math.floor(array.length / 2)));
 
-        // Sort the right side 
-        let arrayRight = mergeSort(array.slice(Math.floor((array.length)/2), array.length));
+    // Sort the right side
+    let arrayRight = mergeSort(
+      array.slice(Math.floor(array.length / 2), array.length)
+    );
 
-        let pointerLeft = 0; 
-        let pointerRight = 0;
+    let pointerLeft = 0;
+    let pointerRight = 0;
 
-
-        // merge the two halves
-        for (i = 0; i < array.length; i++) {
-            if (arrayLeft[pointerLeft] < arrayRight[pointerRight] || arrayRight[pointerRight] === undefined) {
-                sortedArray[i] = arrayLeft[pointerLeft];
-                pointerLeft++;
-            } else {
-                sortedArray[i] = arrayRight[pointerRight];
-                pointerRight++;
-            }
-        }
-        return sortedArray; 
+    // merge the two halves
+    for (i = 0; i < array.length; i++) {
+      if (
+        arrayLeft[pointerLeft] < arrayRight[pointerRight] ||
+        arrayRight[pointerRight] === undefined
+      ) {
+        sortedArray[i] = arrayLeft[pointerLeft];
+        pointerLeft++;
+      } else {
+        sortedArray[i] = arrayRight[pointerRight];
+        pointerRight++;
+      }
     }
+    return sortedArray;
+  }
 }
-
